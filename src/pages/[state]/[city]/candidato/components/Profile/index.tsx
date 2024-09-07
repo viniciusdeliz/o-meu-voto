5f1b57816cc25f8db9863a3b98f2bb22d4568a62
Vinicius de Liz <vinicius.deliz@gmail.com>
2024-09-07 02:38:55 -0300

    feat: ensure city name is capitalized instead of forced to lowercase in seo and share links description of Candidate page

--- src/pages/[state]/[city]/candidato/components/Profile/index.tsx
+++ src/pages/[state]/[city]/candidato/components/Profile/index.tsx
@@ -11,7 +11,7 @@ import {
   TiktokLogo,
   LinkSimple
 } from '@phosphor-icons/react'
-
+import { capitalizeString } from 'utils/mask'
 import styles from './styles.module.scss'
 
 const icons: Record<TypeLink, React.ReactNode> = {
@@ -53,9 +53,10 @@ export const Profile = (candidate: Candidate) => {
   const code = candidate.numero.toString().split('')
 
   const seoTitle = `Candidato(a) ${candidate.nomeUrna} - Eleições 2024`
-  const seoDescription = `Confira os dados do candidato(a) que está concorrendo ao cargo de ${candidate.cargo.nome.toLocaleLowerCase()} no município de ${candidate.localCandidatura.toLocaleLowerCase()} pelo partido ${
-    candidate.partido.sigla
-  }`
+  const seoDescription = `Confira os dados do candidato(a) que está concorrendo ao cargo de
+    ${candidate.cargo.nome.toLocaleLowerCase()} no município de
+    ${capitalizeString(candidate.localCandidatura)} pelo partido
+    ${candidate.partido.sigla}`
 
   return (
     <div className={classNames('card', styles.profile)}>
