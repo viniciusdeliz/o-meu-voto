9089a32ddf8b3da718ea60ea8ae51ff5608e9f47
Vinicius de Liz <vinicius.deliz@gmail.com>
2024-09-07 02:52:31 -0300

    refactor: spacing of seoDescription

--- src/pages/[state]/[city]/candidato/components/Profile/index.tsx
+++ src/pages/[state]/[city]/candidato/components/Profile/index.tsx
@@ -54,9 +54,9 @@ export const Profile = (candidate: Candidate) => {
 
   const seoTitle = `Candidato(a) ${candidate.nomeUrna} - Eleições 2024`
   const seoDescription = `Confira os dados do candidato(a) que está concorrendo ao cargo de
-    ${candidate.cargo.nome.toLocaleLowerCase()} no município de
-    ${capitalizeString(candidate.localCandidatura)} pelo partido
-    ${candidate.partido.sigla}`
+  ${candidate.cargo.nome.toLocaleLowerCase()} no município de
+  ${capitalizeString(candidate.localCandidatura)} pelo partido
+  ${candidate.partido.sigla}`
 
   return (
     <div className={classNames('card', styles.profile)}>
